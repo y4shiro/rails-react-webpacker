cf3cc9805657e3c5d370d3cb0b560f0ec2970f14
y4shiro <yamashiro_naoki@ca-adv.co.jp>
2018-12-07 16:50:22 +0900

    home/index 作成

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
-class HomeController < ApplicationController
+class HomeController < ActionController::Base
+  include ActionController::RequestForgeryProtection
+  include ActionController::ImplicitRender
+  include ActionView::Layouts
+
+  layout "application"
+
   def index
   end
 end
