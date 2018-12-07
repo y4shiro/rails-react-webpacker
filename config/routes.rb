297214bfde6953ea577716e029e970b7bd315a84
y4shiro <yamashiro_naoki@ca-adv.co.jp>
2018-12-07 18:54:26 +0900

    task controller api に CRUD アクション追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api, {format: 'json'} do
+    namespace :v1 do
+      resources :tasks
+    end
+  end
+
   root 'home#index'
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
